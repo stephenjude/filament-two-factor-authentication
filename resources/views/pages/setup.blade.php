66ee4e509b10bd7d1b8be6dc0f1473a2cdba009b
Elisha Ukpong <ishukpong418@gmail.com>
2024-10-27 07:47:12 +0100

    fixed dark mode support and dashboard link bug

--- resources/views/pages/setup.blade.php
+++ resources/views/pages/setup.blade.php
@@ -1,10 +1,4 @@
 @php use Stephenjude\FilamentTwoFactorAuthentication\Livewire\TwoFactorAuthentication; @endphp
 <x-filament-panels::page.simple>
     @livewire(TwoFactorAuthentication::class, ['aside' => false, 'redirectTo' => filament()->getCurrentPanel()->getProfileUrl()])
-
-    @if(!filament('filament-two-factor-authentication')->hasEnforcedTwoFactorSetup() || filament()->auth()->user()?->hasEnabledTwoFactorAuthentication())
-        <x-filament::link :href="filament()->getCurrentPanel()->getUrl(filament()->getTenant())" weight="semibold">
-            {{__('Dashboard')}}
-        </x-filament::link>
-    @endif
 </x-filament-panels::page.simple>
