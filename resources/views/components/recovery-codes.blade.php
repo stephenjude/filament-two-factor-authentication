255af73d91e1d125374fd50e364ddf93a7bc20d0
Bartłomiej Gajda <b.gajda@webard.me>
2025-02-28 19:46:31 +0100

    wip

--- resources/views/components/recovery-codes.blade.php
+++ resources/views/components/recovery-codes.blade.php
@@ -1,8 +1,3 @@
-<h2 class="text-xl font-medium mb-4">{{__('You have enabled two factor authentication.')}}</h2>
-
-<p class="text-sm mb-4">
-    {{__('Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.')}}
-</p>
 
 <div class="mb-4 bg-gray-100 dark:bg-gray-800 dark:text-gray-200 p-4 rounded-md">
     @foreach($this->getUser()->recoveryCodes() as $code)
@@ -10,6 +5,3 @@
     @endforeach
 </div>
 
-{{$this->generateNewRecoveryCodes}}
-
-{{$this->disableTwoFactorAuthentication}}
