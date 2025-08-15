f0569623da88dd83ac3c6493136d2a9449929907
Stephen Jude <31182887+stephenjude@users.noreply.github.com>
2025-08-15 10:30:07 +0100

    Revert "ref: Updated UI"

--- resources/views/pages/setup.blade.php
+++ resources/views/pages/setup.blade.php
@@ -1,36 +1,36 @@
 @php
-    use Filament\Models\Contracts\FilamentUser;
+    use Stephenjude\FilamentTwoFactorAuthentication\Livewire\PasskeyAuthentication;
     use Stephenjude\FilamentTwoFactorAuthentication\Livewire\TwoFactorAuthentication;
     use Stephenjude\FilamentTwoFactorAuthentication\TwoFactorAuthenticationPlugin;
+
     $plugin = TwoFactorAuthenticationPlugin::get();
-    /** @var FilamentUser $user */
+
+    /** @var \Filament\Models\Contracts\FilamentUser $user */
     $user = filament()->auth()->user();
-    $panel = filament()->getCurrentPanel();
 @endphp
 <x-filament-panels::page.simple>
 
-	@@ -18,19 +19,24 @@
+    @if($plugin->hasEnabledTwoFactorAuthentication())
+        @livewire(TwoFactorAuthentication::class, ['aside' => false, 'redirectTo' => filament()->getCurrentPanel()->getProfileUrl()])
+    @endif
+
+    @if($plugin->hasEnabledPasskeyAuthentication())
         @livewire(PasskeyAuthentication::class, ['aside' => false])
     @endif
 
-    <div class="flex flex-col sm:flex-row justify-center items-center gap-4 pt-4">
-        @if(!$plugin->hasForcedTwoFactorSetup() || $user->hasEnabledTwoFactorAuthentication())
-            <x-filament::button
-                tag="a"
-                href="{{ $panel->getUrl() }}"
-                icon="heroicon-o-arrow-left"
-                color="gray"
-                size="sm"
-                class="w-full sm:w-auto"
-            >
-                {{ __('filament-two-factor-authentication::section.dashboard') }}
-            </x-filament::button>
+    <div class="text-center">
+        @if(
+            !$plugin->hasForcedTwoFactorSetup() ||
+             $user->hasEnabledTwoFactorAuthentication()
+        )
+            <x-filament::link :href="filament()->getCurrentPanel()->getUrl(filament()->getTenant())"
+                              weight="semibold">
+                {{__('filament-two-factor-authentication::section.dashboard')}}
+            </x-filament::link>
+        @else
+
         @endif
 
-        <x-filament-two-factor-authentication::logout
-            class="w-full sm:w-auto"
-            icon="heroicon-o-arrow-right-start-on-rectangle"
-            size="sm"
-        />
+        <x-filament-two-factor-authentication::logout />
     </div>
 </x-filament-panels::page.simple>
